78a5d619e726374eae0d277af75ee76abd5ecf3f
Paige Kelly <pckelly.co@gmail.com>
2021-01-25 22:11:30 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -91,7 +91,6 @@ end
 def changing_alan
   # change what Alan Kay is :known_for to the value of the alans_new_info variable. 
   # return the entire updated hash
-  alans_new_info = "GUI"
 
 	programmer_hash = 
  		{
@@ -108,6 +107,7 @@ def changing_alan
         :languages => ["C"]
       }
     }
+    alans_new_info = "GUI"
     programmer_hash[:alans_kay][:known_for] = alans_new_info
     programmer_hash
 end
