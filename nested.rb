e4c24efc9ec7f6b25a324241327585e6bea2165d
Paige Kelly <pckelly.co@gmail.com>
2021-01-25 22:07:40 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -15,7 +15,7 @@ def hopper
         :languages => ["C"]
       }
     }
-
+    value_of_grace_hopper = programmer_hash[:grace_hopper]
 end
 
 def alan_kay_is_known_for
@@ -36,7 +36,7 @@ def alan_kay_is_known_for
         :languages => ["C"]
       }
     }
-
+    programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
@@ -55,7 +55,7 @@ def dennis_ritchies_language
         :languages => ["C"]
       }
     }
-
+    programmer_hash[:dennis_ritchie][:languages].join(" ")
 end
 
 def adding_matz
@@ -68,6 +68,10 @@ def adding_matz
 
 	programmer_hash = 
  		{
+ 		  :yukihiro_matsumoto => {
+        :known_for => "Ruby",
+        :languages => ["LISP", "C"]
+      },
       :grace_hopper => {
         :known_for => "COBOL",
         :languages => ["COBOL", "FORTRAN"]
@@ -81,7 +85,7 @@ def adding_matz
         :languages => ["C"]
       }
     }
-
+    programmer_hash
 end
 
 def changing_alan
@@ -103,8 +107,12 @@ def changing_alan
         :languages => ["C"]
       }
     }
-
+    alans_new_info = programmer_hash[:alans_kay][:known_for]
+    alans_new_info = "GUI"
+    puts programmer_hash
 end
+
+changing_alan
 
 def adding_to_dennis
   # add "Assembly" to Dennis Ritchie's languages array
